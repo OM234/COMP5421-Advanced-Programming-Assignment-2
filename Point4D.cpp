f97b6f0b7d9102f8c5f4adc0ed936e65123534cb
Oz <frost57@gmail.com>
2020-05-27 19:04:29 -0400

    implemented more overloads

--- Point4D.cpp
+++ Point4D.cpp
@@ -10,6 +10,7 @@
 using namespace std; //TODO: remove
 
 Point4D result;
+double Point4D::tolerance = 1.0E-6;
 
 Point4D::Point4D() { //default constructor
     point[0] = point[1] = point[2] = point[3] = 0;
@@ -40,12 +41,12 @@ Point4D::Point4D(double arg1, double arg2, double arg3, double arg4) { //4 param
     point[3] = arg4;
 }
 
-Point4D::Point4D(const Point4D& aPoint) {
-    this->point[0] = aPoint.point[0];
-    this->point[1] = aPoint.point[1];
-    this->point[2] = aPoint.point[2];
-    this->point[3] = aPoint.point[3];
-}
+Point4D::Point4D(const Point4D &aPoint) = default; // copy constructor
+//    this->point[0] = aPoint.point[0];
+//    this->point[1] = aPoint.point[1];
+//    this->point[2] = aPoint.point[2];
+//    this->point[3] = aPoint.point[3];
+
 
 Point4D &Point4D::inverse() const { //inverse function
 
@@ -62,15 +63,123 @@ Point4D &Point4D::inverse() const { //inverse function
     return result;
 }
 
-Point4D& Point4D::operator*(const Point4D& aPoint){
+Point4D &operator*(const Point4D &pointA, const Point4D &pointB) {
 
-    result.point[0] = this->point[0] * aPoint.point[0] + this->point[1] * aPoint.point[3];
-    result.point[1] = this->point[0] * aPoint.point[1] + this->point[1] * aPoint.point[2];
-    result.point[2] = this->point[3] * aPoint.point[1] + this->point[2] * aPoint.point[2];
-    result.point[3] = this->point[3] * aPoint.point[0] + this->point[2] * aPoint.point[3];
+    result.point[0] = pointA.point[0] * pointB.point[0] + pointA.point[1] * pointB.point[3];
+    result.point[1] = pointA.point[0] * pointB.point[1] + pointA.point[1] * pointB.point[2];
+    result.point[2] = pointA.point[3] * pointB.point[1] + pointA.point[2] * pointB.point[2];
+    result.point[3] = pointA.point[3] * pointB.point[0] + pointA.point[2] * pointB.point[3];
 
     return result;
 }
+
+Point4D &operator*(double a, const Point4D &aPoint) {
+
+    result.point[0] = a * aPoint.point[0];
+    result.point[1] = a * aPoint.point[1];
+    result.point[2] = a * aPoint.point[2];
+    result.point[3] = a * aPoint.point[3];
+
+    return result;
+}
+
+Point4D &operator*(const Point4D &aPoint, double a) {
+    return a * aPoint;
+}
+
+Point4D &operator+(const Point4D &pointA, const Point4D &pointB){
+    result.point[0] = pointA.point[0] + pointB.point[0];
+    result.point[1] = pointA.point[1] + pointB.point[1];
+    result.point[2] = pointA.point[2] + pointB.point[2];
+    result.point[3] = pointA.point[3] + pointB.point[3];
+
+    return result;
+}
+
+Point4D &operator+(double a, const Point4D& pointA){ //scalar addition operator overload
+    result.point[0] = pointA.point[0] + a;
+    result.point[1] = pointA.point[1] + a;
+    result.point[2] = pointA.point[2] + a;
+    result.point[3] = pointA.point[3] + a;
+
+    return result;
+}
+
+Point4D &operator+(const Point4D& pointA, double a) { //scalar addition operator overload
+    return a + pointA;
+}
+
+Point4D &operator-(const Point4D & pointA, const Point4D& pointB) { //binary subtraction operator overload
+
+    result.point[0] = pointA.point[0] - pointB.point[0];
+    result.point[1] = pointA.point[1] - pointB.point[1];
+    result.point[2] = pointA.point[2] - pointB.point[2];
+    result.point[3] = pointA.point[3] - pointB.point[3];
+
+    return result;
+}
+
+Point4D &operator-(double a, const Point4D & pointA) { //scalar subtraction operator overload
+
+    result.point[0] = a - pointA.point[0];
+    result.point[1] = a - pointA.point[1];
+    result.point[2] = a - pointA.point[2];
+    result.point[3] = a - pointA.point[3];
+
+    return result;
+}
+Point4D &operator-(const Point4D & pointA, double a) { //scalar subtraction operator overload
+    result.point[0] = pointA.point[0] - a;
+    result.point[1] = pointA.point[1] - a;
+    result.point[2] = pointA.point[2] - a;
+    result.point[3] = pointA.point[3] - a;
+
+    return result;
+}
+
+bool Point4D::operator==(const Point4D &aPoint) {
+    Point4D tempSubtraction;
+    double absSubVal;
+
+    tempSubtraction.point[0] = this->point[0] - aPoint.point[0];
+    tempSubtraction.point[1] = this->point[1] - aPoint.point[1];
+    tempSubtraction.point[2] = this->point[2] - aPoint.point[2];
+    tempSubtraction.point[3] = this->point[3] - aPoint.point[3];
+
+    absSubVal = std::abs(tempSubtraction.point[0]) + std::abs(tempSubtraction.point[1])
+                + std::abs(tempSubtraction.point[2]) + std::abs(tempSubtraction.point[3]);
+
+    return absSubVal <= tolerance;
+}
+
+Point4D &Point4D::operator=(const Point4D &) = default; //assignment operator overload
+
+Point4D &Point4D::operator+() { //unary plus operator overload
+    return *this;
+}
+
+Point4D &Point4D::operator-() { //unary minus operator overload
+
+    *this = *this * -1;
+    return *this;
+}
+
+Point4D &Point4D::operator++() { //pre-increment operator overload
+
+}
+
+void Point4D::setTolerance(double tol) {
+    tolerance = std::abs(tol);
+}
+
+double Point4D::getTolerance() {
+    return tolerance;
+}
+
+//
+//const array<double, 4> Point4D::getPoint() const {
+//    return point;
+//}
 
 std::string Point4D::toString() const { //toString function
 
@@ -87,9 +196,8 @@ std::ostream &operator<<(std::ostream &sout, const Point4D &aPoint) {
 int main() {
 
     Point4D m1(2, 3, 4, 5);
+    Point4D m2(4, 6, 8, 10);
     //cout<<"hello" <<endl;
-
-    Point4D m2 = m1.inverse();
-    Point4D m3 = m1 * m2;
-    cout << m3;
+    Point4D m3 = m1 - 5.45;
+    cout<< m3;
 }
--- Point4D.h
+++ Point4D.h
@@ -11,8 +11,9 @@ using namespace std;
 
 class Point4D {
 
-public:  //TODO: Change to private
+private:
     array<double, 4> point;
+    static double tolerance;
 
 public:
     Point4D();  //default constructor
@@ -22,11 +23,36 @@ public:
     Point4D(double, double, double, double); // 4 parameter constructor
     Point4D(const Point4D &); //copy constructor
     Point4D &inverse() const;
-    Point4D &operator*(const Point4D&);
+    friend Point4D &operator*(const Point4D &, const Point4D &);
+    friend Point4D &operator*(double, const Point4D &);
+    friend Point4D &operator+(const Point4D &, const Point4D &); //binary addition operator overload
+    friend Point4D &operator+(double, const Point4D &); //scalar addition operator overload
+    friend Point4D &operator-(const Point4D &, const Point4D &); //binary subtraction operator overload
+    friend Point4D &operator-(double, const Point4D &); //scalar subtraction operator overload
+    friend Point4D &operator-(const Point4D &, double); //scalar subtraction operator overload
+    bool operator==(const Point4D &);
+    Point4D &operator=(const Point4D &); //assignment operator overload
+    Point4D &operator+(); //unary plus operator overload
+    Point4D &operator-();//unary minus operator overload
+    Point4D &operator++(); //pre-increment operator overload
+    Point4D &operator++(int); //pose-increment operator overload
+    static void setTolerance(double tol);
+
+    static double getTolerance();
+
+    //const std::array<double, 4> getPoint() const;
     std::string toString() const;
 };
 
 std::ostream &operator<<(std::ostream &, const Point4D &); //<< operator overload
-
+Point4D &operator*(const Point4D &, const Point4D &); // multiplication operator overload
+Point4D &operator*(double, const Point4D &); //scalar multiplication operator overload
+Point4D &operator*(const Point4D &, double); //scalar multiplication operator overload
+Point4D &operator+(const Point4D &, const Point4D &); //binary addition operator overload
+Point4D &operator+(double, const Point4D &); //scalar addition operator overload
+Point4D &operator+(const Point4D &, double); //scalar addition operator overload
+Point4D &operator-(const Point4D &, const Point4D&); //binary subtraction operator overload
+Point4D &operator-(double, const Point4D &); //scalar subtraction operator overload
+Point4D &operator-(const Point4D &, double); //scalar subtraction operator overload
 
 #endif //COMP5421_ADVANCED_PROGRAMMING_ASSIGNMENT_2_POINT4D_H
